f25fbd4d1d5d196e62f80dbea7fb5f01eaea5bbc
Ching-Hsin Lee <chinglee@amazon.com>
2022-10-04 18:14:28 +0800

    Revert test debug

--- FreeRTOS-Plus/Demo/FreeRTOS_Cellular_Interface_Windows_Simulator/MQTT_Mutual_Auth_Demo_with_HL7802/cellular_config.h
+++ FreeRTOS-Plus/Demo/FreeRTOS_Cellular_Interface_Windows_Simulator/MQTT_Mutual_Auth_Demo_with_HL7802/cellular_config.h
@@ -51,7 +51,7 @@
 #endif
 
 #ifndef LIBRARY_LOG_LEVEL
-    #define LIBRARY_LOG_LEVEL    LOG_DEBUG
+    #define LIBRARY_LOG_LEVEL    LOG_INFO
 #endif
 
 /* Prototype for the function used to print to console on Windows simulator
